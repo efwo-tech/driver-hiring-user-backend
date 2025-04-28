f67932e0219d983cfe38713644b5f752fe854d4b
Raga2707 <ragamalya295@gmail.com>
2025-04-29 00:51:19 +0530

    first commit

---
+++ src/main/java/com/efwo/driver_hiring_user_backend/DriverHiringUserBackendApplication.java
@@ -0,0 +1,13 @@
+package com.efwo.driver_hiring_user_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DriverHiringUserBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DriverHiringUserBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/efwo/driver_hiring_user_backend/DriverHiringUserBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.efwo.driver_hiring_user_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DriverHiringUserBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
